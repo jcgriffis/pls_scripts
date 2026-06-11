c3fa99c4bdb75dd549ccc8b4cbc305de5336c287
jcgriffis <jcgriffis@uiowa.edu>
2026-06-11 08:54:27 -0500

    Delete PLSR_Code/PLSR_Code directory

--- PLSR_Code/PLSR_Code/Support_Functions/get_has_dmg.m
+++
@@ -1,7 +0,0 @@
-function [X_dmg_only, X_has_dmg] = get_has_dmg(X, bin_thresh, d_thresh)
-
-X_bin = X; X_bin(X_bin >= bin_thresh)=1; X_bin(X_bin<1)=0; % binarize damaged vs. not damaged
-X_has_dmg = (nansum(X_bin,1)); X_has_dmg = find(X_has_dmg > d_thresh); % identify damaged connections
-X_dmg_only = [X(:,X_has_dmg)]; % remove undamaged connections from X
-
-end
